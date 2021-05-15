95511018252bc8b6f5a620a42ecf7653b0a7700c
NikolayHD <hidalgo1984@list.ru>
2021-05-15 20:59:11 +0300

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -6,13 +6,13 @@ function append_stylesheet(stylesheet) {
 
 append_stylesheet(`
 .table tbody>tr>td>div {
-  line-height: normal;
+  line-height: normal!important;
 }
 .table tbody>tr>td, .UICard {
-  padding: 1px 2px;
+  padding: 1px 2px!important;
 }
 .table tr {
-  height: auto;
+  height: auto!important;
 }
 body,
 html,
@@ -23,151 +23,154 @@ html,
 .pt-dark .pt-select select,
 .pt-dark table.pt-html-table,
 [class*="src-modules-SecuritySummary-components-Fundamentals-components-FundamentalsChart-styles-title-"] {
-  font-size: 11px;
+  font-size: 11px!important;
 }
 [class*="src-modules-Orders-containers-components-styles-main-"] {
-  min-width: 100%;
+  min-width: 100%!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"]>div:not(:last-of-type) {
-  margin-right: 6px;
+  margin-right: 6px!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"]>div,
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"] {
-  flex-shrink: 1;
+  flex-shrink: 1!important;
 }
 [class*="src-modules-Timeline-containers-components-TimelineList-styles-main-"] {
-  margin-top: 2px; padding: 2px 1px 4px!important;
+  margin-top: 2px!important;
+  padding: 2px 1px 4px!important;
 }
 [class*="src-components-SmartList-styles-withMargin-"] {
-  margin: 0 2px;
+  margin: 0 2px!important;
 }
 [class*="src-modules-SecuritySummary-containers-SecuritySummary-securityInfoContainer-"] {
-  padding: 1px 2px 4px;
+  padding: 1px 2px 4px!important;
 }
 [class*="src-modules-Orders-containers-styles-inner-"] {
-  margin: 0 2px;
+  margin: 0 2px!important;
 }
 [class*="src-modules-SecuritySummary-components-Fundamentals-components-FundamentalsChart-Chart-styles-wrapper-"] {
-  height: 86px;
+  height: 86px!important;
 }
 [class*="src-modules-Orderbook-containers-components-styles-row-"] {
-  padding: 0px 1px;
+  padding: 0px 1px!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"] {
-  white-space: break-spaces;
+  white-space: break-spaces!important;
 }
 div:first-child>[class*="src-modules-SecuritySummary-components-Fundamentals-components-FundamentalsChart-styles-title-"] {
-  margin-top: 2px;
+  margin-top: 2px!important;
 }
 [class*="src-modules-SecuritySummary-components-Fundamentals-components-FundamentalsChart-styles-title-"] {
-  margin-top: 17px;
-  padding-bottom: 0px;
+  margin-top: 17px!important;
+  padding-bottom: 0px!important;
 }
 [class*="src-components-SecurityInfo-SecurityInfo-positionInfo-"],
 [class*="src-components-TickerInfo-TickerInfo-container-"] {
-  padding: 1px 0;
+  padding: 1px 0!important;
 }
 [class*="src-components-TickerInfo-TickerInfo-firstColumn-"] {
-  margin-right: 0px;
+  margin-right: 0px!important;
 }
 [class*="src-containers-Space-styles-widgetsWrap-1-"] {
-  margin: 0;
+  margin: 0!important;
 }
 [class*="src-modules-Orders-containers-components-styles-main-"] {
-  margin-top: 0px; margin-bottom: 2px;
+  margin-top: 0px!important;
+  margin-bottom: 2px!important;
 }
 [class*="src-modules-Orders-containers-components-styles-header-"] {
-  padding-bottom: 0px;
+  padding-bottom: 0px!important;
 }
 [class*="src-containers-Profile-styles-wallet-"] {
-  padding: 2px 2px;
+  padding: 2px 2px!important;
 }
 [class*="src-components-MenuWidget-styles-btnWrapper-"],
 [class*="src-containers-Profile-styles-wallet-"],
 [class*="src-containers-Dashboard-styles-header-"] {
-  height: auto;
+  height: auto!important;
 }
 [class*="src-containers-Dashboard-styles-footer-"],
 [class*="src-containers-SpacesTabs-styles-tabBtn-"] {
-  padding: 0; height: auto;
+  padding: 0!important;
+  height: auto!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-font-"]:nth-child(2) [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-bold-"]:nth-child(2),
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-font-"]:nth-child(4) [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-bold-"]:nth-child(2) {
   font-size: 28px!important;
-  white-space: nowrap;
+  white-space: nowrap!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-font-"]:nth-child(3),
 [class*="src-modules-Portfolio-containers-components-MarginStatus-MarginStatus-container-"] {
-  display: none;
+  display: none!important;
 }
 [class*="src-containers-WidgetLayout-styles-widget-"] {
-  padding-top: 0px;
+  padding-top: 0px!important;
 }
 [class*="src-modules-SecuritySummary-components-Fundamentals-components-FundamentalsChart-Chart-styles-max-"] {
-  margin: 0px;
+  margin: 0px!important;
 }
 [class*="src-modules-SecuritySummary-components-SummaryTabs-styles-tabsContainer-"] div[role=tab] {
-  line-height: 14px;
-  height: 20px;
+  line-height: 14px!important;
+  height: 20px!important;
 }
 [class*="src-modules-Dividends-components-Filters-Filters-container-"] {
-  padding: 0;
+  padding: 0!important;
 }
 [class*="src-modules-Dividends-components-Filters-Filters-container-"] {
-  height: auto;
+  height: auto!important;
 }
 .table thead>tr>th {
-  padding: 0 1px;
+  padding: 0 1px!important;
 }
 [class*="src-components-TabPanel-styles-wrapper-"] {
-  padding: 0;
+  padding: 0!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-container-"] {
   padding: 0!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-BrandInfo-BrandInfo-container-"]>div {
-  margin-bottom: 1px;
+  margin-bottom: 1px!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-styles-topMargin-"] {
-  margin-top: 1px;
+  margin-top: 1px!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-styles-wrapper-"] {
-  padding: 0;
+  padding: 0!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-BrandInfo-BrandInfo-text-"]>p {
-  margin: 0;
+  margin: 0!important;
 }
 [class*="src-modules-Dividends-components-Table-DividendsTable-wrapper-"] {
-  height: calc(100% - 48px);
+  height: calc(100% - 48px)!important;
 }
 
 /* instrument summary */
 [class*="src-modules-SecuritySummary-containers-SecuritySummary-assetCardContainer-"] {
-  display: none;
+  display: none!important;
 }
 [class*="src-containers-WidgetLayout-styles-widget-"] {
-  padding-bottom: 0;
+  padding-bottom: 0!important;
 }
 [class*="src-modules-ChartTV-containers-styles-TVChartContainer-"] {
-  margin-bottom: 0;
+  margin-bottom: 0!important;
 }
 [class*="src-components-Table-styles-title-"] {
-  color: transparent;
-  background-color: #48aff0;
-  height: 2px;
-  opacity: 0.3;
+  color: transparent!important;
+  background-color: #48aff0!important;
+  height: 2px!important;
+  opacity: 0.3!important;
 }
 [class*="src-components-Table-styles-arrow-"] {
-  display: none;
+  display: none!important;
 }
 [class*="src-modules-Timeline-containers-components-TimelineList-styles-actionTitle-"] {
-  font-size: 11px;
+  font-size: 11px!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-BrandInfo-BrandInfo-text-"] {
-  line-height: normal;
+  line-height: normal!important;
 }
 [class*="src-modules-SecuritySummary-components-SummaryTabs-styles-tabsContainer-"] div[role=tab] {
-  height: 2px;
+  height: 2px!important;
 }
 [class*="src-modules-SecuritySummary-components-SummaryTabs-styles-tabsContainer-"] div[role=tab],
 [class*="src-modules-SecuritySummary-components-SummaryTabs-styles-tabsContainer-"] div[role=tab][aria-selected=true],
@@ -177,23 +180,23 @@ div:first-child>[class*="src-modules-SecuritySummary-components-Fundamentals-com
 
 /*стоимость (в рублях)*/
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"]>div:nth-child(2)>div:first-child {
-  font-size: 0;
+  font-size: 0!important;
 }
 [class*="src-modules-Portfolio-containers-components-PortfolioHeader-PortfolioHeader-main-2-"]>div:nth-child(2)>div:first-child > :first-child {
-  font-size: 13px;
+  font-size: 13px!important;
 }
 [class*="src-modules-Market-containers-styles-table-"] thead>tr>th:first-child {
   width: auto!important;
 }
 .table tbody>tr>td:first-of-type {
-  max-width: 300px;
+  max-width: 300px!important;
 }
 
 /* stock exchange & country icon */
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-iconContainer-"],
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-secondaryTextContainer-"],
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-secondaryText-"] {
-  display: none;
+  display: none!important;
 }
 
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-container-"],
@@ -202,94 +205,110 @@ div:first-child>[class*="src-modules-SecuritySummary-components-Fundamentals-com
   display: inline!important;
 }
 [class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-container-"] {
-  margin-right: 10px;
+  margin-right: 10px!important;
 }
 
 /* edit order form */
 [class*="src-components-Forms-commonStyles-label-"] {
-  margin-bottom: 0;
+  margin-bottom: 0!important;
 }
 [class*="src-modules-CombinedOrder-components-OrderSummary-OrderSummary-orderSummary-"] {
-  margin-top: 0;
-  padding: 0;
+  margin-top: 0!important;
+  padding: 0!important;
 }
 [class*="src-modules-Orders-containers-components-Editing-EditForm-actions-"] {
-  padding-top: 0;
+  padding-top: 0!important;
 }
 [class*="src-modules-CombinedOrder-components-OrderSummary-OrderSummary-row-"] {
-  padding: 0;
+  padding: 0!important;
 }
 [class*="src-modules-Orders-containers-components-Editing-EditForm-container-"] {
-  padding: 0;
+  padding: 0!important;
 }
 
 /*order line*/
 [class*="src-modules-Orders-containers-components-styles-bottom-"] {
-  padding: 0;
+  padding: 0!important;
 }
 
 /*about instrument*/
 [class*="rc-modules-SecuritySummary-components-SummaryTabs-styles-container-"] div[role=tabpanel].pt-tab-panel {
-  height: 100%;
+  height: 100%!important;
+}
+[class*="src-modules-SecuritySummary-components-CompanyDescription-components-BrandInfo-BrandInfo-container-"] {
+  display: inline!important;
+}
+[class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-icon-"],
+[class*="src-modules-SecuritySummary-components-CompanyDescription-components-InfoCard-InfoCard-icon-"] div {
+  display: inline!important;
+  margin-right: 20px!important;
 }
 
-/* portfolio row height */
+/* icons */
 [class*="src-components-SymbolLogoAndName-SymbolLogoAndName-logo-"],
-[class*="src-modules-Portfolio-containers-styles-logo-"] {
-  height: 16px;
-  width: 16px;
-  min-width: 16px;
-  min-height: 16px;
-  padding: 0;
-  margin-right: 2px;
+[class*="src-modules-Portfolio-containers-styles-logo-"],
+[class*="src-components-Tickers-Tickers-logo-"],
+[class*="src-components-Logo-Logo-logoImage-"],
+[class*="src-components-Logo-Logo-logoLarge-"],
+[class*="src-components-Logo-Logo-logoLarge-"] svg,
+[class*="src-components-Logo-Logo-logoSmall-"]
+{
+  height: 16px!important;
+  width: 16px!important;
+  min-width: 16px!important;
+  min-height: 16px!important;
+  padding: 0!important;
 }
 
 /* news menu */
 [class*="src-modules-Feed-components-NewsList-NewsList-settingsContainer-"] {
-  margin-top: 0;
+  margin-top: 0!important;
 }
 [class*="src-components-VirtualList-VirtualList-updateButton-"] {
-  margin: 0;
-}
-[class*="src-components-Tickers-Tickers-logo-"] {
-  height: 16px;
-  width: 16px;
-  min-width: 16px;
-  min-height: 16px;
-  padding: 0;
+  margin: 0!important;
 }
 [class*="src-modules-Feed-components-NewsList-ShortNewsItem-tickers-"] {
-  margin-bottom: 0px;
-  margin-right: 4px;
-  display: inline-flex;
+  margin-bottom: 0px!important;
+  margin-right: 4px!important;
+  display: inline!important;
 }
+[class*="src-components-Logo-Logo-logoSmall-"] {
+  display: inline!important;
+  margin-right: 4px!important;
+  margin-left: 0!important;
+}
+[class*="src-components-Tickers-Tickers-name-"] {
+  display: inline!important;
+  margin: 0!important;
+}
+[class*="src-components-Tickers-Tickers-restNumber-"],
 [class*="src-modules-Feed-components-NewsList-ShortNewsItem-title-"] {
-  display: inline;
+  display: inline!important;
 }
 [class*="src-modules-Feed-components-NewsList-ShortNewsItem-details-"] {
-  display: inline;
-  margin-top: 0px;
+  display: inline!important;
+  margin-top: 0px!important;
 }
 [class*="src-modules-Feed-components-NewsList-NewsList-newsItem-"] {
-  margin-bottom: 0px;
+  margin-bottom: 0px!important;
 }
 [class*="src-modules-Feed-components-NewsList-ShortNewsItem-header-"] {
-  display: inline;
-  margin-left: 4px;
+  display: inline!important;
+  margin-left: 4px!important;
 }
 
 /* all page */
 #SpaceVisibleArea {
-  margin-left: 0;
+margin-left: 0!important;
 }
 [class*="packages-core-lib-containers-Space-styles-space-"] {
-  height: calc(100vh - 46px);
+  height: calc(100vh - 46px)!important;
 }
 
 /* orderbook row */
 [class*="src-modules-Orderbook-containers-components-styles-cell-"] {
-  padding: 0!important;
-  line-height: 1.15;
+  padding: 0!important!important;
+  line-height: 1.15!important;
 }
 [class*="src-modules-Orderbook-containers-components-OrdersTag-OrdersTag-orderTag-"] {
   line-height: unset!important;
@@ -303,13 +322,13 @@ div:first-child>[class*="src-modules-SecuritySummary-components-Fundamentals-com
 }
 [class*="packages-core-lib-components-SpaceTab-styles-remove-"],
 [class*="packages-core-lib-containers-SpacesTabs-styles-addBtn-"] {
-  padding: 0;
-  height: auto;
+  padding: 0!important;
+  height: auto!important;
 }
 
 [class*="src-containers-App-styles-footer-"] {
-  padding: 0;
-  height: auto;
+  padding: 0!important;
+  height: auto!important;
 }
 
 /* top-level scrollbars */
